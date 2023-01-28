b1547df2c5af9d9f73c0aca7dac1ddfa6e95cab0
Tony <tonymelek.au@gmail.com>
2023-01-28 16:45:17 +1100

    initial commit

---
+++ prisma/migrations/20230126103456_init/migration.sql
@@ -0,0 +1,21 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "email" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "BookMark" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "bookName" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "link" TEXT NOT NULL,
+
+    CONSTRAINT "BookMark_pkey" PRIMARY KEY ("id")
+);
---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Post } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+
+@Controller('auth')
+export class AuthController{
+    constructor(private authService:AuthService){}
+    @Post('signup')
+    signup(){
+        return 'I am sign up'
+    }
+    @Post('signin')
+    signin(){
+        return ' I am sign in'
+    }
+}
